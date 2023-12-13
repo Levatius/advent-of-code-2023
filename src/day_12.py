4dfce8ea8814bcf688ab8a5339d28f08c20d9ed4
Lev <levatius@hotmail.co.uk>
2023-12-13 21:09:32 +0000

    Added caches

--- src/day_12.py
+++ src/day_12.py
@@ -1,40 +1,49 @@
 from functools import lru_cache
 
-import regex
+from more_itertools import windowed
 
 
 def get_records(lines, folds):
     spring_records = []
-    condition_records = []
+    group_records = []
     for line in lines:
-        springs, conditions = line.split(" ")
-        # Springs
+        springs, groups = line.split(" ")
         springs = "?".join(springs for _ in range(folds))
         spring_records.append(springs)
-        # Conditions
-        conditions = [int(condition) for condition in conditions.split(",")]
-        conditions *= folds
-        condition_records.append(conditions)
-    return zip(spring_records, tuple(condition_records))
+        groups = tuple(int(group) for group in groups.split(","))
+        groups *= folds
+        group_records.append(groups)
+    return zip(spring_records, group_records)
 
 
 @lru_cache
-def calc_combinations(springs, conditions):
-    total = 0
-    for match in regex.finditer(fr"([#?]{{{conditions[0]}}})", springs, overlapped=True):
-        i, j = match.regs[1]
-        skipped_springs, next_springs = springs[:i], springs[j:]
-        if "#" in skipped_springs or (len(next_springs) and next_springs[0] == "#"):
+def get_next_springs(springs, group_size):
+    next_springs_list = []
+    for start, sub_springs in enumerate(windowed(springs, group_size)):
+        if None in sub_springs or "#" in springs[:start]:
+            # Unrecoverable states, break
+            break
+        end = start + group_size
+        next_springs = springs[end:]
+        if "." in sub_springs or (next_springs and next_springs[0] == "#"):
+            # Recoverable states, continue
             continue
-        if next_conditions := conditions[1:]:
-            total += calc_combinations(next_springs[1:], next_conditions)
+        next_springs_list.append(next_springs[1:])
+    return next_springs_list
+
+
+@lru_cache
+def calc_combinations(springs, groups):
+    total = 0
+    for next_springs in get_next_springs(springs, groups[0]):
+        if next_groups := groups[1:]:
+            # Move onto the next groups
+            total += calc_combinations(next_springs, next_groups)
         elif "#" not in next_springs:
+            # If we still have broken springs remaining, our match has failed
             total += 1
     return total
 
 
 def part_x(records):
-    total = 0
-    for spring_records, condition_records in records:
-        total += calc_combinations(spring_records, tuple(condition_records))
-    return total
+    return sum(calc_combinations(springs, groups) for springs, groups in records)
